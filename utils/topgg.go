9b7e8cd26c6b985a13482fad5204308eaccf8ec3
Ludoviko <me@ludoviko.ch>
2022-03-26 08:40:57 +0000

    Added more error handling, hopefully this avoids the panic.
    Timeout is now 60s so it should reduce the chance of a timeout.
    Won't know if this fixes #3 until the API has issues again.

--- utils/topgg.go
+++ utils/topgg.go
@@ -10,22 +10,33 @@ import (
 
 func SendTopggData(token string, botId string, shards uint, dctoken string) {
 	httpClient := &http.Client{
-		Timeout: 10 * time.Second,
+		Timeout: 60 * time.Second,
 	}
 
-	reqBody, _ := json.Marshal(map[string]uint{
+	reqBody, err := json.Marshal(map[string]uint{
 		"server_count": uint(GetConnectedServerCount(dctoken)),
 		"shard_count": shards,
 	})
 
-	req, _ := http.NewRequest("POST", "https://top.gg/api/bots/" + botId + "/stats", bytes.NewBuffer(reqBody))
+	if err != nil {
+		fmt.Println("Error marshalling top.gg data:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "https://top.gg/api/bots/" + botId + "/stats", bytes.NewBuffer(reqBody))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Add("Authorization", token)
+
+	if err != nil {
+		fmt.Println("Error starting request, top.gg data:", err)
+		return
+	}
 
 	resp, err := httpClient.Do(req)
 
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	defer resp.Body.Close()
