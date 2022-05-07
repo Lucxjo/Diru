5ee1cabc7fb3da44fde6bc1eba708beb35315ea7
Ludoviko <me@ludoviko.ch>
2022-05-07 14:34:45 +0100

    Updated files to work with Google service accounts

--- cfg/config.go
+++ cfg/config.go
@@ -16,15 +16,17 @@ type Topgg struct {
 type DiruConfig struct {
 	DiscordToken string `default:""`
 	DeeplToken   string `default:""`
+	GtrToken     string `default:""`
+	GtrProjectId string `default:""`
 	Topgg Topgg
 }
 
 func GetConfig() DiruConfig {
 
 	if _, err := os.Stat("config/Diru.json"); errors.Is(err, os.ErrNotExist) {
-		ioutil.WriteFile("config/Diru.json", []byte("{\n    \"discord_token\": \"\",\n    \"deepl_token\": \"\"\n}"), 0644)
+		ioutil.WriteFile("config/Diru.json", []byte("{\n    \"discord_token\": \"\",\n    \"deepl_token\": \"\"\n,\n    \"gtr_token\": \"\"\n}"), 0644)
 
-		panic("config/Diru.json not found. It has been created for you, you must enter your values for discord_token and deepl_token.")
+		panic("config/Diru.json not found. It has been created for you, you must enter your values for discord_token, gtr_token, and gtr_project_id and deepl_token.")
 	}
 
 	var cfg DiruConfig
@@ -42,6 +44,8 @@ func GetConfig() DiruConfig {
 	config := DiruConfig{
 		DiscordToken: cfg.DiscordToken,
 		DeeplToken:   cfg.DeeplToken,
+		GtrToken:     cfg.GtrToken,
+		GtrProjectId: cfg.GtrProjectId,
 		Topgg: cfg.Topgg,
 	}
 
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+GOOGLE_APPLICATION_CREDENTIALS=./config/gcloud.json go run diru.go
