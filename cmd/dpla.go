456c1041de3b2cf7eb1a8bf0c81025066c4c6bc0
Tim Hugall <39146315+TimHugall@users.noreply.github.com>
2022-06-04 15:48:49 +1000

    Let Dpla handle absent prefix

--- cmd/dpla.go
+++ cmd/dpla.go
@@ -15,8 +15,13 @@ func Dpla(msg *disgord.Message, s disgord.Session, c *deeplgo.Client) {
 		text := msg.ReferencedMessage.Content
 
 		msg.Reply(context.Background(), s, deepl.AutoTranslate(text, c))
-	} else {
+	} else if strings.Contains(msg.Content, "dpl") {
 		text := strings.Split(msg.Content, " ")[2:]
+		tx := strings.Join(text, " ")
+		msg.Reply(context.Background(), s, deepl.AutoTranslate(tx, c))
+	} else {
+		// Because this is the option if no prefix is chosen, we need to start from index 1
+		text := strings.Split(msg.Content, " ")[1:]
 		tx := strings.Join(text, " ")
 		msg.Reply(context.Background(), s, deepl.AutoTranslate(tx, c))
 	}
