8be6244e5b357e8bde231417e5c22c184ad857cb
Louis Hollingworth <louis@hollingworth.ch>
2022-07-30 15:19:18 +0100

    (#29,#30) Add simple command for dpla
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- src/bot/commands/general/translate.ts
+++ src/bot/commands/general/translate.ts
@@ -5,22 +5,38 @@ import {
 	EmbedBuilder,
 	PermissionsBitField,
 } from 'discord.js';
-import { Discord, Slash, SlashGroup, SlashOption } from 'discordx';
+import {
+	Discord,
+	SimpleCommand,
+	SimpleCommandMessage,
+	SimpleCommandOption,
+	Slash,
+	SlashGroup,
+	SlashOption,
+} from 'discordx';
 import { SecureConnect } from '../../../shared/SecureConnect';
 
 @Discord()
 @SlashGroup({ name: 'translate', description: 'Translate text' })
 @SlashGroup('translate')
 export class Translate {
+	private static embed = new EmbedBuilder()
+		.setTitle('DeepL Translation')
+		.setColor('Aqua');
+
 	@Slash('deepl', { description: 'Translate text using DeepL' })
-	async deepl(
+	async deeplSlash(
 		@SlashOption('phrase', {
 			description: 'The phrase to translate',
 			type: ApplicationCommandOptionType.String,
 			required: true,
 		})
 		phrase: string,
-		@SlashOption('data', { description: 'This will show extra information about a translation', type: ApplicationCommandOptionType.Boolean, required: false })
+		@SlashOption('data', {
+			description: 'This will show extra information about a translation',
+			type: ApplicationCommandOptionType.Boolean,
+			required: false,
+		})
 		data: boolean = false,
 		interaction: CommandInteraction
 	) {
@@ -46,14 +62,10 @@ export class Translate {
 				);
 			});
 
-		const embed = new EmbedBuilder()
-			.setTitle('DeepL Translation')
-			.setColor('Aqua')
-		
 		if (data) {
-			embed.addFields(embedFields);
+			Translate.embed.addFields(embedFields);
 		} else {
-			embed.setDescription(embedFields[2].value);
+			Translate.embed.setDescription(embedFields[2].value);
 		}
 
 		if (
@@ -65,14 +77,68 @@ export class Translate {
 			)
 		) {
 			interaction.reply({
-				embeds: [embed],
+				embeds: [Translate.embed],
 				ephemeral: false,
 			});
 		} else {
 			interaction.reply({
-				embeds: [embed],
+				embeds: [Translate.embed],
 				ephemeral: true,
 			});
 		}
+	}
+
+	@SimpleCommand('dpla', {
+		description: 'Translate text to English (GB) using DeepL',
+		argSplitter: ', ',
+	})
+	async deeplASimple(
+		@SimpleCommandOption('phrase', {
+			description: 'The phrase to be translated',
+		})
+		phrase: string,
+		command: SimpleCommandMessage
+	) {
+		if (!phrase) {
+			command.message.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setTitle('Error')
+						.setDescription(
+							'You need to specify a phrase to translate\nUsage: `@Diru dpl <language>, <phrase>`'
+						)
+						.setColor('Red'),
+				],
+			});
+			return;
+		}
+
+		let embedFields: { name: string; value: string }[] = [
+			{ name: 'Requested phrase:', value: phrase },
+		];
+
+		await axios
+			.post('http://localhost:3000/api/translate/deepl', {
+				text: phrase,
+				KEY: SecureConnect.key,
+			})
+			.then((res) => {
+				embedFields.push(
+					{
+						name: 'Detected language:',
+						value: res.data.detected_source_language,
+					},
+					{
+						name: 'Translated phrase:',
+						value: res.data.text,
+					}
+				);
+			});
+
+		Translate.embed.setDescription(embedFields[2].value);
+
+		command.message.reply({
+			embeds: [Translate.embed],
+		});
 	}
 }
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@ import 'dotenv/config';
 export const { DISCORD_TOKEN, CLIENT_ID, GUILD_ID, DEEPL_TOKEN } = process.env;
 
 export const NODE_ENV = process.env.NODE_ENV ?? 'development';
-export const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/diru';
+export const MONGO_URI =
+	process.env.MONGO_URI ?? 'mongodb://localhost:27017/diru';
